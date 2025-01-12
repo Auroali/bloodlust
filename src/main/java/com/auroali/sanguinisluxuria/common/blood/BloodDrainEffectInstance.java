65eda5e105fdb7131870d35b8cf67280769f0ed5
Auroali <dawn.auroali@gmail.com>
2025-01-12 19:39:55 -0400

    prevent negative duration/amplifier and chances outside of 0-1 in blood drain effects

--- src/main/java/com/auroali/sanguinisluxuria/common/blood/BloodDrainEffectInstance.java
+++ src/main/java/com/auroali/sanguinisluxuria/common/blood/BloodDrainEffectInstance.java
@@ -5,13 +5,14 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.registry.Registries;
+import net.minecraft.util.dynamic.Codecs;
 
 public record BloodDrainEffectInstance(StatusEffect effect, int duration, int amplifier, float chance) {
     public static final Codec<BloodDrainEffectInstance> CODEC = RecordCodecBuilder.create(instance -> instance.group(
       Registries.STATUS_EFFECT.getCodec().fieldOf("effect").forGetter(BloodDrainEffectInstance::effect),
-      Codec.INT.optionalFieldOf("duration", 300).forGetter(BloodDrainEffectInstance::duration),
-      Codec.INT.optionalFieldOf("amplifier", 0).forGetter(BloodDrainEffectInstance::amplifier),
-      Codec.FLOAT.optionalFieldOf("chance", 1.f).forGetter(BloodDrainEffectInstance::chance)
+      Codecs.NONNEGATIVE_INT.optionalFieldOf("duration", 300).forGetter(BloodDrainEffectInstance::duration),
+      Codecs.NONNEGATIVE_INT.optionalFieldOf("amplifier", 0).forGetter(BloodDrainEffectInstance::amplifier),
+      Codec.floatRange(0.f, 1.f).optionalFieldOf("chance", 1.f).forGetter(BloodDrainEffectInstance::chance)
     ).apply(instance, BloodDrainEffectInstance::new));
 
     public static BloodDrainEffectInstance merge(BloodDrainEffectInstance first, BloodDrainEffectInstance second) {
